68585a7bf18ce2d89b3b9d218a5f985bf848cc7a
DJA <DJA@DJA-PC>
2018-02-05 18:42:47 +0900

    tt

--- iot_spring/src/main/java/com/iot/spring/common/aspect/LogPrintAspect.java
+++ iot_spring/src/main/java/com/iot/spring/common/aspect/LogPrintAspect.java
@@ -1,21 +1,33 @@
 package com.iot.spring.common.aspect;
 
+import java.io.IOException;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.iot.spring.dao.NaverTransDAO;
+import com.iot.spring.vo.NaverMsg;
 
 
 @Service
 @Aspect
 public class LogPrintAspect {
 
+	@Autowired
+	@Qualifier("ntdao")
+	private NaverTransDAO ntdao;
+	
 	private static final Logger log = LoggerFactory.getLogger(LogPrintAspect.class);
 	@Before("execution(* com.iot.spring.controller.*Controller.*(..))")
 	public void beforeLog(JoinPoint jp) {
@@ -23,7 +35,7 @@ public class LogPrintAspect {
 	}
 	
 	@Around("execution(* com.iot.spring.controller.*Controller.*(..))")
-	public Object aroundLog(ProceedingJoinPoint pjp) {
+	public Object aroundLog(ProceedingJoinPoint pjp) throws IOException {
 		log.info("@Around begin");
 		Object obj = null;
 		long startTime = System.currentTimeMillis();
@@ -32,7 +44,9 @@ public class LogPrintAspect {
 		} catch (Throwable e) {
 			log.error("error => {}", e);
 			ModelAndView mav = new ModelAndView("error/error");
-			mav.addObject("errorMsg", e.getMessage());
+			ObjectMapper om = new ObjectMapper();
+			NaverMsg nm = om.readValue(e.getMessage(), NaverMsg.class);
+			mav.addObject("errorMsg", ntdao.getText(nm.getMessage().getResult().getTranslatedText()));
 			return mav;
 		}
 		log.info("@Around end, RunTime : {} ms", (System.currentTimeMillis()-startTime));
--- iot_spring/src/main/java/com/iot/spring/controller/EmpController.java
+++ iot_spring/src/main/java/com/iot/spring/controller/EmpController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.iot.spring.dao.NaverTransDAO;
 import com.iot.spring.service.EmpService;
 import com.iot.spring.vo.Emp;
 
